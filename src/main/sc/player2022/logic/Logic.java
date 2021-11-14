288b022a38f1a88b9c6cac95b8416b0ebcb7888e
Niklas101 <avragedotexe@gmail.com>
2021-11-14 20:36:36 +0100

    w

--- src/main/sc/player2022/logic/Logic.java
+++ src/main/sc/player2022/logic/Logic.java
@@ -29,8 +29,7 @@ public class Logic implements IGameHandler {
 //TEST
   @Override
   public Move calculateMove() {
-    //kommentar von robin
-    //kommentar 2 von robin
+
     long startTime = System.currentTimeMillis();
     log.info("Es wurde ein Zug von {} angefordert.", gameState.getCurrentTeam());
 
