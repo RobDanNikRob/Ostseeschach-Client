4653d659c6b47d83f0ba5c08ece9f2a55781ab6a
RoboLP <RoboLP@users.noreply.github.com>
2021-11-14 15:34:48 +0100

    Hallo test

--- src/main/sc/player2022/logic/Logic.java
+++ src/main/sc/player2022/logic/Logic.java
@@ -29,7 +29,7 @@ public class Logic implements IGameHandler {
 
   @Override
   public Move calculateMove() {
-    //wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+    //hallo test
     long startTime = System.currentTimeMillis();
     log.info("Es wurde ein Zug von {} angefordert.", gameState.getCurrentTeam());
 
