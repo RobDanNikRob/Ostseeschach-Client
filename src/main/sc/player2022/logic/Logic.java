ae4bebbda5307f3f47561a273c3e1d81a39a84af
Niklas101 <avragedotexe@gmail.com>
2021-11-14 20:33:01 +0100

    w

--- src/main/sc/player2022/logic/Logic.java
+++ src/main/sc/player2022/logic/Logic.java
@@ -26,7 +26,7 @@ public class Logic implements IGameHandler {
   public void onGameOver(GameResult data) {
     log.info("Das Spiel ist beendet, Ergebnis: {}", data);
   }
-//TESTO
+//TESTOoppozrthihzratinalefunktionsrhgntdffgdh
   @Override
   public Move calculateMove() {
     //kommentar von robin
